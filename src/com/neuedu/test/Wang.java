e4bbcc397114bd7e9e77cdf124afa60fb706431c
tianjiaojiao131415 <1877519658@qq.com>
2020-02-10 19:47:09 +0800

    多态

---
+++ src/com/neuedu/test/Wang.java
@@ -0,0 +1,11 @@
+package com.neuedu.test;
+
+/**
+ * Created by admin on 2020/2/9.
+ */
+public class Wang extends Teacher {
+    @Override
+    public void teach() {
+        System.out.println("上课");
+    }
+}
