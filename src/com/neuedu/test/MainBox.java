fd3987723393303681d5ab92152d74105f934f63
tianjiaojiao131415 <1877519658@qq.com>
2020-02-10 19:59:32 +0800

    0206

---
+++ src/com/neuedu/test/MainBox.java
@@ -0,0 +1,1 @@
+package com.neuedu.test;/** * Created by admin on 2020/2/6. */public  class MainBox {    private Cpu cpu;    public Cpu getCpu() {        return cpu;    }    public void setCpu(Cpu cpu) {        this.cpu = cpu;    }    public void method(){        this.cpu.method();    }}
